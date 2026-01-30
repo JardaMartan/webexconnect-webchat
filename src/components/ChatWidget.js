cd33b6d0c300b72c475051eb6576276a87dc8cd5
Jaroslav Martan <jmartan@cisco.com>
2026-01-30 16:38:05 +0100

    Refactor Back and Close buttons to native HTML buttons for stability

--- src/components/ChatWidget.js
+++ src/components/ChatWidget.js
@@ -204,11 +204,11 @@ export class ChatWidget extends HTMLElement {
     if (this.view === 'list') {
       headerHtml = `
           <span>${this.i18n.t('my_chats')}</span>
-          <md-button variant="ghost" size="28" circle class="close-btn" id="closeBtn">
+          <button class="icon-btn close-btn" id="closeBtn">
             <svg xmlns="http://www.w3.org/2000/svg" style="width:20px;height:20px;fill:currentColor;" viewBox="0 0 24 24">
                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
             </svg>
-          </md-button>
+          </button>
       `;
       const threadsHtml = this.threads.map(t => `
         <md-list-item slot="list-item" class="thread-item" data-id="${t.id}">
@@ -233,18 +233,18 @@ export class ChatWidget extends HTMLElement {
     } else {
       headerHtml = `
         <div class="header-left">
-          <md-button variant="ghost" size="28" circle class="back-btn">
+          <button class="icon-btn back-btn">
              <svg xmlns="http://www.w3.org/2000/svg" style="width:20px;height:20px;fill:currentColor;" viewBox="0 0 24 24">
                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
              </svg>
-          </md-button>
+          </button>
           <span>${this.i18n.t('chat_header')}</span>
         </div>
-        <md-button variant="ghost" size="28" circle class="close-btn" id="closeBtn">
-            <svg xmlns="http://www.w3.org/2000/svg" style="width:20px;height:20px;fill:currentColor;" viewBox="0 0 24 24">
-               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-            </svg>
-        </md-button>
+        <button class="icon-btn close-btn" id="closeBtn">
+          <svg xmlns="http://www.w3.org/2000/svg" style="width:20px;height:20px;fill:currentColor;" viewBox="0 0 24 24">
+             <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+          </svg>
+        </button>
       `;
       contentHtml = `
         <div class="message-list">
